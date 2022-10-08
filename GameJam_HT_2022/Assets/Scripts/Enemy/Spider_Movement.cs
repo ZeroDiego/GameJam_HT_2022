4ee4a80af81a95068a03a2993e243bf25ff748a6
IsakLundin <82143860+IsakLundin@users.noreply.github.com>
2022-10-08 20:01:32 +0200

    clean spidermovement

--- GameJam_HT_2022/Assets/Scripts/Enemy/Spider_Movement.cs
+++ GameJam_HT_2022/Assets/Scripts/Enemy/Spider_Movement.cs
@@ -6,47 +6,22 @@ using UnityEngine;
 public class Spider_Movement : MonoBehaviour
 {
 
-    //public GameObject target;
     private JamSpawner spawner;
 
     public float moveSpeed = 1f;
     public float rotationSpeed = 4f;
-    private bool isAlive = true;
-    //private bool jamExist = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //target = GameObject.FindGameObjectWithTag("Jam");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         FindClosestJam();
-
-        /*target = GameObject.FindGameObjectWithTag("Jam");
-        if(target == null)
-        {
-            target = GameObject.FindGameObjectWithTag("Player");
-        }*/
-
-        /*Vector2 direction = target.transform.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle+90, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);*/
     }
-
-    /*private void FixedUpdate()
-    {
-        if (isAlive == true)
-        {
-            if (target == true)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
-            }
-        }
-    }*/
 
     private void FindClosestJam()
     {
